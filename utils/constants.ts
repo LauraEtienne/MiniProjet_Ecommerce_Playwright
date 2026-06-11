f84f0a7aa755d2ae3661db3464068ba857372fa8
Laura ETIENNE <pro.laura.etienne@gmail.com>
2026-06-11 12:04:06 -0400

    POM acceuil et constants

---
+++ utils/constants.ts
@@ -0,0 +1,23 @@
+export const URLS = {
+//Liens de la page d'accueil
+decouvrirLesProduitsoduits: /.*products/,
+enSavoirPlus: /.*category-gaming/,
+accessoires: /.*category-office/,
+maisonConnectee: /.*products/,
+gaming: /.*category-gaming/,
+bureau: /.*category-office/,
+voirTout: /.*products/,
+voirleCatalogue: /.*category-gaming/,
+
+//Liens du header
+headerAccueil: /.*shop.missionplaywright.fr/,
+headerProduits: /.*produits/,
+headerApropos: /.*about/,
+headerContact: /.*contact/,
+//headerSearch: /.*category-gaming/,
+headerCart: /.*cart/,
+headerLogin: /.*auth/
+
+};
+
+
