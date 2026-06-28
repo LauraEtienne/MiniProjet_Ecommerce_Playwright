2447f7386072a4ef2dd6beeff778ecc97cbbdf0d
Laura ETIENNE <pro.laura.etienne@gmail.com>
2026-06-28 07:05:24 -0400

    Ajout POM checkout

--- tests/pages/checkout.ts
+++ tests/pages/checkout.ts
@@ -24,6 +24,7 @@ export class Checkout {
     readonly editionDateExpiration: Locator;
     readonly editionCVV: Locator;
     readonly btnSoumettrePaiment: Locator;
+    readonly totalAmountLabel: Locator;
 
     // Page3
     readonly titreConfirmation: Locator;
@@ -31,6 +32,7 @@ export class Checkout {
     readonly confirmationCard: Locator;
     readonly btnSuivreMaCommande: Locator;
     readonly btnContinuerMesAchats: Locator;
+    readonly numeroCommande: Locator;
 
 
 
@@ -49,53 +51,73 @@ export class Checkout {
         this.btnContinuerVersPaiment = page.getByTestId('shipping-submit-button');
         // Page2
         this.titrePaiement = page.getByTestId('cart-count');
-        this.editionNumeroCarte = page.;
+        this.editionNumeroCarte = page.getByTestId('payment-cardnumber-input');
         this.editionNomCarte = page.getByTestId('clear-cart-button');
         this.editionDateExpiration = page.getByTestId('checkout-button');
         this.editionCVV = page.getByTestId('continue-shopping-button');
         this.btnSoumettrePaiment = page.getByTestId('payment-submit-button');
+        this.totalAmountLabel = page.locator('span.text-2xl.font-bold');
         // Page3
         this.titreConfirmation = page.getByRole('heading', { name: 'Commande confirmée !' });
         this.messageSucces = page.getByText('Merci pour votre commande.');
         this.confirmationCard = page.getByTestId('order-confirmation-card');
         this.btnSuivreMaCommande = page.getByTestId('track-order-button');
         this.btnContinuerMesAchats = page.getByTestId('continue-shopping-button');
+        this.numeroCommande = page.getByTestId('order-number');
 
     }
 
     //méthodes TODO
-    // Augmenter quantité produit
-    async augmenterQteProduit (idProduit: string) {
-        await this.page.getByTestId('increase-quantity-${idProduit}')
+    // Saisir les données de la page Livraison et passer au paiement
+    async soumettreDonneesLivraison(prenom: string, nom: string, email: string, telephone: string, adresse: string, ville: string, codepostal: string) {
+        await this.editionPrenom.fill(prenom),
+        await this.editionNom.fill(nom),
+        await this.editionEmail.fill(email),
+        await this.editionTelephone.fill(telephone),
+        await this.editionAdresse.fill(adresse),
+        await this.editionVille.fill(ville),
+        await this.editionCodePostal.fill(codepostal),
+        await this.btnContinuerVersPaiment.click()
 }
 
-    // Diminuer quantité produit
-    async diminuerQteProduit (idProduit: string) {
-        await this.page.getByTestId('decrease-quantity-${idProduit}');
-    
-}
+    // Saisir les données de la page paiement et cliquer sur le CTA Payer
+    async soumettreDonneesPaiement(numCarte: string, nomcarte: string, dateExp: string, CVV: string) {
+        await this.editionNumeroCarte.fill(numCarte),
+        await this.editionNomCarte.fill(nomcarte),
+        await this.editionDateExpiration.fill(dateExp),
+        await this.editionCVV.fill(CVV),
+        await this.btnSoumettrePaiment.click()
 
-    // Supprimer produit
-    async supprimerProduit (idProduit: string) {
-        await this.page.getByTestId('remove-item-${idProduit}')
-    
-}
+    }
 
-    // Vider le panier
-    async viderLePanier () {
-        await this.btnViderLePanier.click();
-    
-}
-    // Passer au paiement
-    async passerAuPaiement () {
-        await this.btnPasserAuPaiement.click();
-    
-}
+    // Retourner le montant total à payer pour test de vérification
+    async retournerMontantAPayer () : Promise<string> {
+        const text = await this.totalAmountLabel.textContent();
+       //Si text contient bien une chaîne de caractères (vrai), alors remplace les retours à la ligne et les espaces multiples par un seul espace propre
+       // Sinon (si text est null ou undefined), alors renvoie une chaîne vide ''.        
+       return text ? text.replace(/\s+/g, ' ').trim() : ''; 
+       // Résultat propre : "149.99 €"
+
+    }
+
+    // Retourner le numéro de commande
+    async retournenmeroCommandxe() : Promise<string>  {
+       const text = await this.numeroCommande.textContent();
+       //Si text contient bien une chaîne de caractères (vrai), alors remplace les retours à la ligne et les espaces multiples par un seul espace propre
+       // Sinon (si text est null ou undefined), alors renvoie une chaîne vide ''.
+       return text ? text.replace(/\s+/g, ' ').trim() : ''; 
+
+    }
+    // Suivre ma commande
+    async suivreCommande():Promise<void> {
+        await this.btnSuivreMaCommande.click();
+
+    }
 
     // Continuer mes achats
-    async continuerMesAchats () {
+    async continuerMesAchats():Promise<void> {
         await this.btnContinuerMesAchats.click();
-    
-}
+
+    }
 
 }
