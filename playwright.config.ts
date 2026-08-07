99dcc53c8e6610ba9c3c280b0d6ba8b5d7ff0998
Laura ETIENNE <pro.laura.etienne@gmail.com>
2026-08-07 14:52:08 -0400

    ci: add slack reporter integration

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+  ['html'],
+  [
+    'playwright-slack-report',
+    {
+      slackWebHookUrl: process.env.SLACK_WEBHOOK_URL,
+      sendResults: 'always',
+    },
+  ],
+],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -85,8 +94,6 @@ export default defineConfig({
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
-
- 
 
 
 });
